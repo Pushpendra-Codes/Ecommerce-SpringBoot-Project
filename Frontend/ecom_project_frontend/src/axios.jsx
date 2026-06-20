08e78eb279afa34fde4031b23bfe145364ae49b5
Pushpendra-Codes <pushpendra.lap@gmail.com>
2026-06-20 19:48:16 +0530

    Change API baseURL to production endpoint

--- Frontend/ecom_project_frontend/src/axios.jsx
+++ Frontend/ecom_project_frontend/src/axios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  // baseURL: "http://localhost:8080/api",
+  baseURL: "https://ecommerce-springboot-project-11.onrender.com/api",
 });
 delete API.defaults.headers.common["Authorization"];
 export default API;
