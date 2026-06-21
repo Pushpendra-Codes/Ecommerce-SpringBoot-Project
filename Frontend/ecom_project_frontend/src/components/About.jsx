ef54faf1d5bef4d0beb0ead2f638ceecaaa955b2
Pushpendra-Codes <pushpendra.lap@gmail.com>
2026-06-21 13:56:17 +0530

    Add About component with developer information

---
+++ Frontend/ecom_project_frontend/src/components/About.jsx
@@ -0,0 +1,20 @@
+const About = () => {
+  return (
+    <div className="container text-center mt-5 pt-5">
+      <h2>About Developer</h2>
+      <h4>Pushpendra Patidar</h4>
+      <p>
+        Instagram:
+        <a
+          href="https://instagram.com/your_instagram_username"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}@your_instagram_username
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default About;
