655f4002ae047ed1006c4113a1397952f2715f9f
Pushpendra-Codes <pushpendra.lap@gmail.com>
2026-06-21 13:08:31 +0530

    Update product image API endpoint

--- Frontend/ecom_project_frontend/src/components/Home.jsx
+++ Frontend/ecom_project_frontend/src/components/Home.jsx
@@ -23,7 +23,7 @@ const Home = ({ selectedCategory }) => {
           data.map(async (product) => {
             try {
               const response = await axios.get(
-                `https://localhost:8080/api/product/${product.id}/image`,
+                `https://ecommerce-springboot-project-11.onrender.com/api/product/${product.id}/image`,
                 { responseType: "blob" }
               );
               const imageUrl = URL.createObjectURL(response.data);
