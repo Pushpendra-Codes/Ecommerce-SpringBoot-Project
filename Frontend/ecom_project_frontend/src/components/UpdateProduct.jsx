405d45fbff8087753941b3cb5ae5ce08d6d0a346
Pushpendra-Codes <pushpendra.lap@gmail.com>
2026-06-21 13:11:29 +0530

    Update API endpoints for product fetching and updating

--- Frontend/ecom_project_frontend/src/components/UpdateProduct.jsx
+++ Frontend/ecom_project_frontend/src/components/UpdateProduct.jsx
@@ -22,13 +22,13 @@ const UpdateProduct = () => {
     const fetchProduct = async () => {
       try {
         const response = await axios.get(
-          `https://localhost:8080/api/product/${id}`
+          `https://ecommerce-springboot-project-11.onrender.com/api/product/${id}`
         );
 
         setProduct(response.data);
       
         const responseImage = await axios.get(
-          `https://localhost:8080/api/product/${id}/image`,
+          `https://ecommerce-springboot-project-11.onrender.com/api/product/${id}/image`,
           { responseType: "blob" }
         );
        const imageFile = await converUrlToFile(responseImage.data,response.data.imageName)
@@ -67,7 +67,7 @@ const UpdateProduct = () => {
 
   console.log("formData : ", updatedProduct)
     axios
-      .put(`https://localhost:8080/api/product/${id}`, updatedProduct, {
+      .put(`https://ecommerce-springboot-project-11.onrender.com/api/product/${id}`, updatedProduct, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
