5196c5e75758b0a2afb57e6050976754448ffc75
Pushpendra-Codes <pushpendra.lap@gmail.com>
2026-06-21 15:09:02 +0530

    Refactor About component layout and content
    Updated the About component to include a structured layout with sections for personal introduction and social links.

--- Frontend/ecom_project_frontend/src/components/About.jsx
+++ Frontend/ecom_project_frontend/src/components/About.jsx
@@ -1,39 +1,52 @@
+import "./About.css";
+
 const About = () => {
   return (
-    <div
-      style={{
-        minHeight: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <div
-        style={{
-          padding: "40px",
-          borderRadius: "15px",
-          background: "#1e1e1e",
-          color: "white",
-          textAlign: "center",
-          boxShadow: "0 8px 20px rgba(0,0,0,0.3)",
-        }}
-      >
-        <h1>About Developer</h1>
-        <h3>Pushpendra Patidar</h3>
+    <section className="about-page">
+      <div className="about-left">
+        <p className="hello">Hello 👋</p>
 
-        <a
-          href="https://www.instagram.com/pushpendra_patidar16/"
-          target="_blank"
-          rel="noreferrer"
-          style={{
-            textDecoration: "none",
-            fontSize: "18px",
-          }}
-        >
-          Instagram: @pushpendra_patidar16
-        </a>
+        <h2>
+          I'm <span>Pushpendra Patidar</span>
+        </h2>
+
+        <h1>Software Developer</h1>
+
+        <p className="desc">
+          Passionate Full Stack Developer who enjoys building scalable web
+          applications using React, Spring Boot, and PostgreSQL.
+        </p>
+
+        <div className="buttons">
+          <a
+            href="https://www.instagram.com/pushpendra_patidar16/"
+            target="_blank"
+            rel="noreferrer"
+            className="btn-primary"
+          >
+            Instagram
+          </a>
+
+          <a
+            href="https://github.com/pushpendra-codes"
+            target="_blank"
+            rel="noreferrer"
+            className="btn-secondary"
+          >
+            GitHub
+          </a>
+        </div>
       </div>
-    </div>
+
+      <div className="about-right">
+        <div className="circle">
+          <div className="name-card">
+            <h2>Pushpendra</h2>
+            <p>Full Stack Developer</p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
