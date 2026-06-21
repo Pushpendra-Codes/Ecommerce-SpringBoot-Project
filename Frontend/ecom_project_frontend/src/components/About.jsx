d24040b001df1cc0cfa3f2fc0a6e42886773b79b
Pushpendra-Codes <pushpendra.lap@gmail.com>
2026-06-21 14:17:01 +0530

    Work on the about UI

--- Frontend/ecom_project_frontend/src/components/About.jsx
+++ Frontend/ecom_project_frontend/src/components/About.jsx
@@ -1,21 +1,3 @@
-const About = () => {
-  return (
-    <div className="container text-center mt-5 pt-5">
-      <h2>About Developer</h2>
-      <h4>Pushpendra Patidar</h4>
-      <p>
-        Instagram:
-        <a
-          href="https://www.instagram.com/pushpendra_patidar16/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}@Pushpendra_Patidar16
-        </a>
-      </p>
-    </div>
-  );
-};
 
 const About = () => {
   return (
